7aaa2c0c9af7e73357a67a32f08f9f7647f1d8c0
Anvesha Khandelwal <anveshak0906@gmail.com>
2026-03-20 23:53:20 +0530

    backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 const path = require('path');
 const fs = require('fs');
 
